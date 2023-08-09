073aeb11c4933c05b04d261f5c3fc7d1c6da0769
SurKaa <suralcarywn@qq.com>
2023-08-09 16:18:07 +0800

    引入Element-UI并使用

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 // 清除默认样式
 import '@/style/reset.scss'
+// 引入Element-UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
     router,
