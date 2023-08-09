10bec58c6d33278450577992ea47b3da07d508d7
SurKaa <suralcarywn@qq.com>
2023-08-09 15:59:10 +0800

    引入sass和sass-loader并清空默认样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 清除默认样式
+import '@/style/reset.scss'
 
 Vue.config.productionTip = false
 
