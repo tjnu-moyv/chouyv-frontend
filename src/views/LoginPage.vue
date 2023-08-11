71bd6ea52628c49694fdc4c6785cae028ee88264
SurKaa <suralcarywn@qq.com>
2023-08-11 19:15:04 +0800

    整合登陆注册表单的校验规则

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -2,17 +2,13 @@
   <!-- 登录页面 -->
   <div id="login">
     <h2 class="title">登录</h2>
-    <el-form :model="loginForm" ref="loginForm" label-width="80px" class="form">
-      <el-form-item label="用户名" prop="username" :rules="[
-        { required: true, message: '请输入用户名', trigger: 'blur' }
-      ]">
+    <el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="80px" class="form">
+      <el-form-item label="用户名" prop="username">
         <el-input v-model="loginForm.username" autocomplete="off" placeholder="请输入用户名"></el-input>
       </el-form-item>
-      <el-form-item label="密码" prop="password" :rules="[
-        { required: true, message: '请输入密码', trigger: 'blur' },
-        { min: 6, message: '密码不能少于6位', trigger: 'blur' }
-      ]">
-        <el-input type="password" v-model="loginForm.password" autocomplete="off" show-password placeholder="请输入密码"></el-input>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="loginForm.password" autocomplete="off" show-password
+                  placeholder="请输入密码"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="login" class="login-btn">登录</el-button>
@@ -31,6 +27,16 @@ export default {
       loginForm: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'},
+          {min: 6, message: '密码不能少于6位', trigger: 'blur'},
+          {pattern: /[!-z]/, message: '密码包含了其他字符', trigger: 'blur'}
+        ],
       }
     }
   },
@@ -44,16 +50,16 @@ export default {
             password: this.loginForm.password
           };
           request.post('/students/login', formData)
-            .then(response => {
-              // 登录成功的处理逻辑
-              console.log('登录成功', response);
-              // 可以进行页面跳转或其他操作
-            })
-            .catch(error => {
-              // 登录失败的处理逻辑
-              console.log('登录失败', error);
-              // 可以进行错误提示等操作
-            });
+              .then(response => {
+                // 登录成功的处理逻辑
+                console.log('登录成功', response);
+                // 可以进行页面跳转或其他操作
+              })
+              .catch(error => {
+                // 登录失败的处理逻辑
+                console.log('登录失败', error);
+                // 可以进行错误提示等操作
+              });
         } else {
           console.log('登录失败');
           return false;
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -2,22 +2,15 @@
   <!-- 注册页面 -->
   <div id="register">
     <h2 class="title">注册</h2>
-    <el-form :model="registerForm" ref="registerForm" label-width="80px" class="form">
+    <el-form :model="registerForm" :rules="rules" ref="registerForm" label-width="80px" class="form">
       <el-form-item label="用户名" prop="username">
         <el-input v-model="registerForm.username" autocomplete="off" placeholder="请输入用户名"></el-input>
       </el-form-item>
-      <el-form-item label="密码" prop="password" :rules="[
-        { required: true, message: '请输入密码', trigger: 'blur' },
-        { min: 6, message: '密码不能少于6位', trigger: 'blur' },
-        { pattern: /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, message: '密码必须包含英文和数字', trigger: 'blur' }
-      ]">
+      <el-form-item label="密码" prop="password">
         <el-input type="password" v-model="registerForm.password" autocomplete="off" show-password
                   placeholder="请输入密码"></el-input>
       </el-form-item>
-      <el-form-item label="确认密码" prop="confirmPassword" :rules="[
-        { required: true, message: '请再次输入密码', trigger: 'blur' },
-        { validator: confirmPasswordValidator, trigger: 'blur' }
-      ]">
+      <el-form-item label="确认密码" prop="confirmPassword">
         <el-input type="password" v-model="registerForm.confirmPassword" autocomplete="off" show-password
                   placeholder="请再次输入密码"></el-input>
       </el-form-item>
@@ -37,6 +30,28 @@ export default {
         username: '',
         password: '',
         confirmPassword: ''
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'},
+          {min: 6, message: '密码不能少于6位', trigger: 'blur'},
+          {pattern: /[!-z]/, message: '密码包含了其他字符', trigger: 'blur'}
+        ],
+        confirmPassword: [
+          {required: true, message: '请再次输入密码', trigger: 'blur'},
+          {
+            validator: (rule, value, callback) => {
+              if (value !== this.registerForm.password) {
+                callback(new Error('两次输入的密码不一致'));
+              } else {
+                callback();
+              }
+            }, trigger: 'blur'
+          }
+        ]
       }
     }
   },
@@ -54,13 +69,6 @@ export default {
     },
     goToLogin() {
       this.$router.push('/login');
-    },
-    confirmPasswordValidator(rule, value, callback) {
-      if (value !== this.registerForm.password) {
-        callback(new Error('两次输入的密码不一致'));
-      } else {
-        callback();
-      }
     }
   }
 }
