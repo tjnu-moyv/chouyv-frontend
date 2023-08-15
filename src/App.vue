4a8541ec1fab2d7555a7889507844df1b2005e01
SurKaa <suralcarywn@qq.com>
2023-08-15 10:51:22 +0800

    Update App.vue
    默认背景以及垂直居中

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,13 @@
     <router-view/>
   </div>
 </template>
+
+<style>
+#app {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(45deg, rgba(64, 158, 255, 0.3), #0b57a6);
+}
+</style>
