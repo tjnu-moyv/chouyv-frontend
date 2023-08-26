70e7426e6fcbd0ddf43f7770847cd26a824312e6
SurKaa <suralcarywn@qq.com>
2023-08-26 15:11:46 +0800

    @ => src配置

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
