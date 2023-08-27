d406e13cf6078bd70dd8637ef9ad3ffeb384d9b2
SurKaa <suralcarywn@qq.com>
2023-08-27 14:30:07 +0800

    学生接口

--- src/api/student/index.ts
+++ src/api/student/index.ts
@@ -1,5 +1,15 @@
 import request from "@/utils/request";
-import type {StudentAuthResponse, StudentLoginDTO, StudentRegisterDTO} from "@/api/student/type";
+import type {
+  ConfirmOrderDTO,
+  ConfirmOrderVO,
+  GenerateOrderDTO,
+  GenerateOrderVO,
+  GetOrderInfoVO,
+  StudentAuthVO,
+  StudentLoginDTO,
+  StudentRegisterDTO
+} from "@/api/student/type";
+import type {StudentInfo} from "@/api/module/StudentInfo";
 
 enum StudentAPI {
   Login = '/students/login',
@@ -8,20 +18,43 @@ enum StudentAPI {
   OrderInfo = '/students/order/',
   GenerateOrder = '/students/order',
   ConfirmOrder = '/students/order/',
-  UpdatePwd = '//students/pwd',
+  // UpdatePwd = '//students/pwd',
 }
 
 /**
  * 登录接口
  * @param {StudentLoginDTO} data
- * @return {StudentAuthResponse}
+ * @return {StudentAuthVO}
  */
-export const requestStudentLogin = (data: StudentLoginDTO) => request.post<any, StudentAuthResponse>(StudentAPI.Login, data)
+export const requestStudentLogin = (data: StudentLoginDTO) => request.post<any, StudentAuthVO>(StudentAPI.Login, data)
 
 /**
  * 注册接口
  * @param {StudentRegisterDTO} data
  */
-export const requestStudentRegister = (data: StudentRegisterDTO) => request.post<any, StudentAuthResponse>(StudentAPI.Register, data)
+export const requestStudentRegister = (data: StudentRegisterDTO) => request.post<any, StudentAuthVO>(StudentAPI.Register, data)
 
-export const requestStudentInfo = () => request.get<any, >()
+/**
+ * 获取学生信息
+ */
+export const requestStudentInfo = () => request.get<any, StudentInfo>(StudentAPI.GetInfo)
+
+/**
+ * 查询订单情况
+ * @param orderId
+ */
+export const requestOrderInfo = (orderId: number) => request.get<any, GetOrderInfoVO>(StudentAPI.OrderInfo + orderId)
+
+/**
+ * 订单生成接口
+ * @param data
+ */
+export const requestGenerateOrder = (data: GenerateOrderDTO) => request.post<any, GenerateOrderVO>(StudentAPI.GenerateOrder, data)
+
+/**
+ * 消费者确认收货
+ * @param data
+ */
+export const requestConfirmOrder = (data: ConfirmOrderDTO) => request.put<any, ConfirmOrderVO>(StudentAPI.ConfirmOrder, data)
+
+// export const requestUpdatePwd = (data: ) => request.post<any, >()
--- src/api/student/type.ts
+++ src/api/student/type.ts
@@ -1,7 +1,9 @@
-import type {ResponseData} from "@/api/module/ResponseData";
+import type {BaseVO} from "@/api/module/BaseVO";
 import type {Auth} from "@/api/module/Auth";
+import type {Order} from "@/api/module/Order";
+import type {OrderProductsItem} from "@/api/module/OrderProductsItem";
 
-export interface StudentLogin {
+export interface StudentLoginDTO {
   username: string,
   password: string
 }
@@ -12,6 +14,45 @@ export interface StudentRegisterDTO {
   checkPwd: string,
 }
 
-export interface StudentAuthResponse extends BaseVO {
+export interface GenerateOrderDTO {
+  /**
+   * 订单的状态-0-堂食(用户取 食堂吃 不外带) 1-带走(打包, 需配送费) 2-找跑腿(run_id不可空)
+   */
+  type: number;
+  shopId: number;
+  products: {
+    count: number;
+    description: string;
+    /**
+     * 商品主键(shop_products_id)
+     */
+    id: number;
+  }[];
+}
+
+export interface ConfirmOrderDTO {
+  orderId: number;
+  password: string;
+}
+
+export interface StudentAuthVO extends BaseVO {
   data: Auth
+}
+
+export interface GenerateOrderVO extends BaseVO {
+  data: {
+    orderId: number;
+    totalPrice: number;
+  }
+}
+
+export interface GetOrderInfoVO extends BaseVO {
+  data: {
+    order: Order;
+    orderShopProductsItems: OrderProductsItem[];
+  }
+}
+
+export interface ConfirmOrderVO extends BaseVO {
+  data: any
 }
