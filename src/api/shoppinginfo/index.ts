a4735359ad47c7c7baa986f45b3b5107d1a50755
SurKaa <suralcarywn@qq.com>
2023-08-27 13:55:18 +0800

    更名

--- src/api/shoppinginfo/index.ts
+++ src/api/shoppinginfo/index.ts
@@ -2,31 +2,31 @@ import request from "@/utils/request";
 import type {AddShoppingInfoDTO, AddShoppingInfoVO, UpdateShoppingInfoDTO} from "@/api/shoppinginfo/type";
 
 enum ShoppingInfoAPI {
-  AddShoppingInfo = '/shopinfo',
-  UpdateShoppingInfo = '/shopinfo',
-  DelShoppingInfo = '/shopinfo',
-  GetAllShoppingInfo = '/shopinfo',
+  Add = '/shopinfo',
+  Update = '/shopinfo',
+  Delete = '/shopinfo',
+  GetAll = '/shopinfo',
 }
 
 /**
  * 增加学生收货地址
  * @param {AddShoppingInfoDTO} data
  */
-export const requestAddShoppingInfo = (data: AddShoppingInfoDTO) => request.post<any, AddShoppingInfoVO>(ShoppingInfoAPI.AddShoppingInfo, data)
+export const requestAddShoppingInfo = (data: AddShoppingInfoDTO) => request.post<any, AddShoppingInfoVO>(ShoppingInfoAPI.Add, data)
 
 /**
  * 更新学生收货地址
  * @param {UpdateShoppingInfoDTO} data
  */
-export const requestUpdateShoppingInfo = (data: UpdateShoppingInfoDTO) => request.put<any, AddShoppingInfoVO>(ShoppingInfoAPI.UpdateShoppingInfo, data)
+export const requestUpdateShoppingInfo = (data: UpdateShoppingInfoDTO) => request.put<any, AddShoppingInfoVO>(ShoppingInfoAPI.Update, data)
 
 /**
  * 删除学生收货地址
  * @param id
  */
-export const requestDelShoppingInfo = (id: number) => request.delete<any, AddShoppingInfoVO>(ShoppingInfoAPI.DelShoppingInfo, id)
+export const requestDelShoppingInfo = (id: number) => request.delete<any, AddShoppingInfoVO>(ShoppingInfoAPI.Delete, id)
 
 /**
  * 删除学生收货地址
  */
-export const requestGetAllShoppingInfo = () => request.get<any, AddShoppingInfoVO>(ShoppingInfoAPI.GetAllShoppingInfo)
+export const requestGetAllShoppingInfo = () => request.get<any, AddShoppingInfoVO>(ShoppingInfoAPI.GetAll)
